77b0997717d44e1a2f8fa35f40322b3878bdd766
Струин Александр Анатольевич <A.Struin@skbkontur.ru>
2022-11-07 14:56:40 +0500

    rollback styles

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -4,8 +4,6 @@ function Main({children}: {children: ReactFragment}){
 
     return (
         <section className="main">
-            <input id="toggle-all" className="toggle-all" type="checkbox" />
-            <label htmlFor="toggle-all" />
             <ul className="todo-list">
                 {children}
             </ul>
